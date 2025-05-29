70d1696d041150bd306ed8cf59b3574336b79e72
Seyedmasih Tabaei <masihtabaei@gmail.com>
2025-05-29 02:30:16 +0200

    fix(unity): hide crosshair on sit
    Hide our crosshair after player sat
    
    Refs: #67

--- unity/desktop-version/Assets/Scripts/ChairInteractionManager.cs
+++ unity/desktop-version/Assets/Scripts/ChairInteractionManager.cs
@@ -4,6 +4,10 @@ public class ChairInteractionManager : MonoBehaviour, IInteractable
 {
     public Camera mainCamera;
     public Camera fixedCamera;
+
+    [SerializeField]
+    private GameObject _crosshair;
+
     public string GetHint()
     {
         return "Press E to sit.";
@@ -11,6 +15,7 @@ public class ChairInteractionManager : MonoBehaviour, IInteractable
 
     public void React()
     {
+        _crosshair.SetActive(false);
         if (fixedCamera != null) fixedCamera.enabled = true;
         if (mainCamera != null) mainCamera.enabled = false;
     }
