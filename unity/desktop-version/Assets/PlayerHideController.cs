ffc3560a2183200c3a5566d32ec1c3e3d77ef711
Seyedmasih Tabaei <masihtabaei@gmail.com>
2025-06-13 01:29:52 +0200

    feat(unity): add hide logic
    Implement an initial version for hiding
    
    Refs: #93

---
+++ unity/desktop-version/Assets/PlayerHideController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class PlayerHideController : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private Camera _hiddenCamera;
+
+    [SerializeField]
+    private GameObject _player;
+
+    private bool _isHidden = false;
+
+
+    public string GetHint()
+    {
+        return "Press E to hide/bloom";
+    }
+
+    public void React()
+    {
+        _isHidden = !_isHidden;
+        if (_hiddenCamera != null) _hiddenCamera.enabled = _isHidden;
+        if (_player != null) _player.gameObject.SetActive(!_isHidden);
+        if (!_isHidden && EventSystem.current != null) EventSystem.current.SetSelectedGameObject(null);
+
+    }
+}
