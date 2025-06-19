adb05f2fb2f79ddd04b0c86b90d81e8c41943405
Jenny <stolerjennifer1@gmail.com>
2025-06-19 09:26:08 +0200

    Fixed Audio Bug

--- unity/desktop-version/Assets/Scripts/HidingHandler.cs
+++ unity/desktop-version/Assets/Scripts/HidingHandler.cs
@@ -10,6 +10,11 @@ public class HidingHandler : MonoBehaviour, IInteractable
     [SerializeField]
     private GameObject _crosshair;
 
+    [SerializeField]
+    private AudioListener _atPlayer;
+    [SerializeField]
+    private AudioListener _atHidingSpot;
+
     public string GetHint()
     {
         return _isHidden ? "Press E to exit!" : "Press E to hide!";
@@ -21,5 +26,7 @@ public class HidingHandler : MonoBehaviour, IInteractable
         _crosshair.SetActive(!_isHidden);
         if (_hiddenCamera != null) _hiddenCamera.enabled = _isHidden;
         if (_mainCamera != null) _mainCamera.enabled = !_isHidden;
+        if (_atPlayer != null) _atPlayer.enabled = !_isHidden;
+        if (_atHidingSpot != null) _atHidingSpot.enabled = _isHidden;
     }
 }
