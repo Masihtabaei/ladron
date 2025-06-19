72eb79317aa925fbb837b36cf65921827dd46843
Seyedmasih Tabaei <masihtabaei@gmail.com>
2025-06-19 19:05:10 +0200

    feat(unity): add progress bar
    Add a simple progress bar to the loading scene
    
    Refs: #4

--- unity/desktop-version/Assets/Scripts/StartMenuManager.cs
+++ unity/desktop-version/Assets/Scripts/StartMenuManager.cs
@@ -1,17 +1,28 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenuManager : MonoBehaviour
 {
     [SerializeField] 
     private GameObject _startMenu;
 
+    [SerializeField] 
+    private GameObject _loading;
+
     [SerializeField]
     private GameObject _credits;
 
+
+    [SerializeField]
+    private Slider _loadingSlider;
+
     public void StartGame()
     {
-        SceneManager.LoadScene("Main");
+        _startMenu.SetActive(false);
+        _loading.SetActive(true);
+        StartCoroutine(LoadGameAsync());
     }
 
     public void ShowCredits()
@@ -29,5 +40,16 @@ public class StartMenuManager : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    IEnumerator LoadGameAsync()
+    { 
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync("Main");
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            _loadingSlider.value = progressValue;
+            yield return null;
+        }
     }
 }
