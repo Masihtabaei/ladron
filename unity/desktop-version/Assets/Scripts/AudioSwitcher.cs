ecd4d940be9492a9585e9aa477bcb2ae9486ae5c
NIKIEMA, Alex Gaetan <alex-gaetan.nikiema@stud.hs-coburg.de>
2025-06-19 17:05:40 +0200

    fix: Merge conflict fixed

--- unity/desktop-version/Assets/Scripts/AudioSwitcher.cs
+++ unity/desktop-version/Assets/Scripts/AudioSwitcher.cs
@@ -30,7 +30,7 @@ public class AudioSwitcher : MonoBehaviour
         bg.clip = elevatorBackground;
         bg.Play();
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(60f);
         bg.Stop();
         bg.loop = false;
         bg.clip = bing;
